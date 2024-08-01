ddb710df8f7cd60f9d3cfacb89ebefeca7aaefda
madzit1 <madzitsadoev1@gmail.com>
2024-08-01 21:45:35 +0300

    fixed errors header

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,7 @@ $(function () {
 
     $(document).ready(function () {
         $('.selectBtn').click(function (evenet) {
-            $('.dropdown').toggleClass('active')
+            $('.main-menu').toggleClass('active')
         });
     });
 });
@@ -27,16 +27,16 @@ $(function () {
 })();
 
 
-$('.card__btn--minus').click(function () {
-    var $input = $(this).parent().parent().find('.card__form-input');
+$('.counter__btn--minus').click(function () {
+    var $input = $(this).parent().parent().find('.counter__form-input');
     var count = parseInt($input.val()) - 1;
     count = count < 1 ? 1 : count;
     $input.val(count);
     $input.change();
     return false;
 });
-$('.card__btn--plus').click(function () {
-    var $input = $(this).parent().parent().find('.card__form-input');
+$('.counter__btn--plus').click(function () {
+    var $input = $(this).parent().parent().find('.counter__form-input');
     $input.val(parseInt($input.val()) + 1);
     $input.change();
     return false;
