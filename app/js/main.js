4503a4a64c6da40b4c9ba302dcc61f2ded674bd8
madzit1 <madzitsadoev1@gmail.com>
2024-09-30 00:13:04 +0300

    adaptive is ready

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,29 @@ $(function () {
             $('.main-menu').toggleClass('active')
         });
     });
-});
+
+    $(document).ready(function () {
+        $('.searchBtn').click(function (evenet) {
+            $('.menu-header__form').toggleClass('active')
+        });
+    });
+
+    $(document).ready(function () {
+        $('.burger-button').click(function (evenet) {
+            $('.backdrop-mobile').addClass('active')
+            $('.body').addClass('lock')
+        });
+    });
+
+    $(document).ready(function () {
+        $('.close-button').click(function (evenet) {
+            $('.backdrop-mobile').removeClass('active')
+            $('.body').removeClass('lock')
+        });
+    });
+
+
+ });
 
 var containerEl1 = document.querySelector('[data-ref="container-1"]');
 var containerEl2 = document.querySelector('[data-ref="container-2"]');
@@ -21,10 +43,28 @@ var mixer2 = mixitup(containerEl2, config);
 
 (() => {
     const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
+        openModalBtn: document.querySelector("[data-basket-open]"),
+        closeModalBtn: document.querySelector("[data-basket-close]"),
         modalBodyBtn: document.querySelector("[data-lock]"),
-        modal: document.querySelector("[data-modal]"),
+        modal: document.querySelector("[data-basket]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        refs.modalBodyBtn.classList.toggle("lock");
+    }
+})();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-basket-mobile-open]"),
+        closeModalBtn: document.querySelector("[data-basket-mobile-close]"),
+        modalBodyBtn: document.querySelector("[data-lock]"),
+        modal: document.querySelector("[data-basket-mobile]"),
     };
 
     refs.openModalBtn.addEventListener("click", toggleModal);
@@ -46,11 +86,13 @@ $('.counter__btn--minus').click(function () {
     $input.change();
     return false;
 });
+
 $('.counter__btn--plus').click(function () {
     var $input = $(this).parent().parent().find('.counter__form-input');
     $input.val(parseInt($input.val()) + 1);
     $input.change();
     return false;
 });
+
 
 
