ffb1a8320c88d51922c5a106be7f223dc06263d2
madzit1 <madzitsadoev1@gmail.com>
2025-02-03 16:10:58 +0200

    ready

--- app/js/main-slider.js
+++ app/js/main-slider.js
@@ -1,6 +1,6 @@
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 0,
     autoHeight: true,
     navigation: {
       nextEl: '.hero__swiper-btn--next',
@@ -21,11 +21,14 @@ var swiper2 = new Swiper(".mySwiper2",{
       delay: 1500,
       disableOnInteraction: false,
     },
+    
   });
+
+
 
 const swiper3 = new Swiper('.mySwiper3', {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 0,
     autoHeight: true,
     navigation: {
       nextEl: '.product__gallery-btn--next',
@@ -44,11 +47,29 @@ const swiper4 = new Swiper('.mySwiper4', {
   slidesPerView: 4,
   spaceBetween: 0,
   autoHeight: true,
+
   navigation: {
     nextEl: '.offers__btn--next',
     prevEl: '.offers__btn--prev',
   },
 
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
 
+    768: {
+      slidesPerView: 3,
+    },
+
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+
+  pagination: {
+    el: ".offers__swiper-pagination",
+    clickable: true,
+  },
 });
  
